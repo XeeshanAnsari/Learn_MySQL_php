66236bea60d2222ab60e57410489169551413bf0
zeeshan <zeeshanansari38244@gmail.com>
2016-07-18 15:37:04 +0500

    User enter value and submit than data Insert in DataBase(MySQL)

---
+++ insert.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inserting Data </title>
+</head>
+<body>
+    <form action="insert.php"  method="post">
+       <table border="5" >
+         <tr bgcolor="yellow"><th colspan="2"><h1>Student Registration</h1></th></tr>
+         <tr>
+         <td>Student Name :</td>
+         <td><input type="text" name="s_name"></td>
+         </tr>
+         <tr>
+         <td>School Name :</td>
+         <td><input type="text" name="school"></td>
+         </tr>
+         <tr>
+         <td>Roll No :</td>
+         <td><input type="text" name="roll"></td>
+         </tr>
+         <tr>
+         <td>Result :</td>
+         <td><input type="text" name="result"></td>
+         </tr>
+         <tr>
+         <td colspan="2"><input type="submit" name="submit" value="submit"></td>
+         </tr>
+       </table>
+    </form>
+</body>
+</html>
+
+
+<?php
+
+    mysql_connect("localhost","root","");
+    mysql_select_db("coching");
+   if(isset($_POST['submit'])){
+      $name = $_POST['s_name'];
+      $school = $_POST['school'];
+      $roll = $_POST['roll'];
+      $result = $_POST['result'];
+
+
+     $query = "insert into student (student_name,school_name,roll_no,result) value('$name','$school','$roll','$result')";  
+     
+     if(mysql_query($query)){
+         echo "<h1>Data Inserted</h1>";
+     }   
+
+   }
+   
+?>
+
+
