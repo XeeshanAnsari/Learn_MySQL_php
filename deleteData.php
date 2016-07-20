0ea7746868beb95bd114f5d9f31243e8f1eced90
zeeshan <zeeshanansari38244@gmail.com>
2016-07-20 10:01:48 +0500

    Delete Data from dataBase

---
+++ deleteData.php
@@ -0,0 +1,15 @@
+<?php
+ 
+   mysql_connect("localhost","root","");
+   mysql_select_db("coching");
+
+   $delete_id = $_GET['del'];
+   $query_d = "DELETE FROM student where id = '$delete_id'";
+   
+   
+   if(mysql_query($query_d)){
+       echo "<script>window.open('delete.php?deleted=Data has been Deleted','_self')</script>" ;
+   } 
+ 
+
+?>
